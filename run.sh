293b2d9d053bd63e34bdb10852e882724497e4fc
Jinwoo Lee <jwlee@autocrypt.io>
2023-01-17 13:03:48 +0900

    draft done

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 npx @marp-team/marp-cli@latest -w main.md --theme ./themes/autocrypt.css
-# npx @marp-team/marp-cli@latest -s ./
+
+# npx @marp-team/marp-cli@latest main.md --theme ./themes/autocrypt.css --pdf
+# npx @marp-team/marp-cli@latest main.md --theme ./themes/autocrypt.css --pptx
